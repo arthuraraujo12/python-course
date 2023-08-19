f93df7bba35c28875f5e1112c0c6b85addca2dc3
Arthur Araujo <arthurmoraes725@gmail.com>
2023-08-19 18:19:39 -0300

    "hello world" multi-language implemented

--- hello.py
+++ hello.py
@@ -1,6 +1,38 @@
-#shebang, com caminhos vituais ->
 #!/usr/bin/env python3
+"""Hello World mult linguage
 
-#Este é o primeiro Hello world em py
-print ("Hello, World")
-print ('Arthur'.upper())
+Depending on the language configured in the environment, the message will 
+change
+
+Usage:
+
+Have the LANG variable properly configure, ex:
+
+    export LANG=pt_BRutf8
+    ou
+    set LANG=pt_BR.utf8
+
+Execution:
+
+    python3 hello.py
+    ou
+    hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Arthur Araujo"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+msg = "Hello, World!"
+if current_language == "pt_BR":
+    msg="Ola Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mundo!"
+elif current_language == "es_SP":
+    msg = "Hola. Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde!"
+
+print (msg)
