ef6bed5c941214cb0cd98402ca1ceea3cd4e6da5
Arthur Araujo <arthurmoraes725@gmail.com>
2023-09-25 16:33:45 -0300

    update 0.1.2

--- hello.py
+++ hello.py
@@ -18,21 +18,20 @@ Execution:
     ou
     hello.py
 """
-__version__ = "0.0.1"
+__version__ = "0.1.2"
 __author__ = "Arthur Araujo"
 __license__ = "Unlicense"
 
 import os
 
 current_language = os.getenv("LANG", "en_US")[:5]
-msg = "Hello, World!"
-if current_language == "pt_BR":
-    msg="Ola Mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao, Mundo!"
-elif current_language == "es_SP":
-    msg = "Hola. Mundo!"
-elif current_language == "fr_FR":
-    msg = "Bonjour Monde!"
 
-print (msg)
+msg = {
+    "en_US": "Hello World!",
+    "pt_BR": "Ola Mundo!",
+    "it_IT": "Ciao, Mundo!",
+    "es_SP": "Hola Mundo!",
+    "fr_FR": "Bonjour Monde",
+}
+
+print (msg[current_language])
