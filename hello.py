a76c7f121f22a968310a1b2dc672d7a920ac243c
Arthur Araujo <arthurmoraes725@gmail.com>
2023-09-25 17:18:35 -0300

    update 0.1.3

--- hello.py
+++ hello.py
@@ -18,13 +18,36 @@ Execution:
     ou
     hello.py
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Arthur Araujo"
 __license__ = "Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
+arguments = {
+    "lang": None,
+    "count": 1,
+}
+for arg in sys.argv[1:]:
+    # TODO: Tartar ValueError anm
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.lstrip("-").strip()
+    if key not in arguments:
+        print(f"Invalid Option {key}")
+        sys.exit()
+    arguments[key] = value
+
+current_language = arguments["lang"]
+if current_language is None:
+#TODO: Usar repetição
+    if "LANG" in os.environ:
+     current_language =  os.getenv("LANG"),
+    else: 
+        current_language = input("Choose a language:")
+
+current_language = current_language[:5]
 
 msg = {
     "en_US": "Hello World!",
@@ -34,4 +57,6 @@ msg = {
     "fr_FR": "Bonjour Monde",
 }
 
-print (msg[current_language])
+print (
+    msg[current_language] * int(arguments["count"])
+    )
